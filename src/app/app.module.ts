a7e3c2b618acc93f3e6e7bf59caaa9ba6966b2c4
Chao Ma <ma.cha@husky.neu.edu>
2018-11-29 02:12:27 -0500

    bind user seleted answers in question_list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { QuestionListComponent } from './question-list/question-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // module for ngModel
   ],
   providers: [],
   bootstrap: [AppComponent]

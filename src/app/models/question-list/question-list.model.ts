acc3f2e931844fc1a1d2a14bfeb24413b739b2e4
Chao Ma <ma.cha@husky.neu.edu>
2018-11-29 22:26:31 -0500

    finish reading and presenting questions from database

--- src/app/models/question-list/question-list.model.ts
+++ src/app/models/question-list/question-list.model.ts
@@ -1,9 +1,14 @@
-export interface Question {
+export interface QuestionList {
     title: string,
-    selectionA: string,
-    selectionB: string,
-    selectionC: string,
-    selectionD: string
-    answer: string;
+    answers: object;
   }
   
+    // [
+    //   {
+    //     "questionText": "Why is the sky blue?", "answers": [
+    //       { "answerText": "blah blah 1", "correct": true },
+    //       { "answerText": "blah blah 2", "correct": false },
+    //       { "answerText": "blah blah 3", "correct": false }
+    //     ]
+    //   }
+    // ]
